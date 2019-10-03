c0272266ee5ab9ba5aa4832a177d52931aec5eab
Peter <pkurjanowicz5@gmail.com>
2019-10-02 20:49:02 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+const headers = {
+    "Accept": "application/json",
+    "app_id": app_id,
+    "app_key": api_key
+}
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/oxfordrequest/:word', (req, res) => {
+request(
+    {url: `https://od-api.oxforddictionaries.com/api/v2/lemmas/en/${req.params.word}` ,
+    headers: headers,
+        },
+    (error, response, body) => {
+        if (error || response.statusCode !== 200) {
+            return res.status(500).json({ type: 'error'});
+    }
+
+        res.json(JSON.parse(body));
+    }
+)
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
---
+++ server.js
@@ -0,0 +1,25 @@
+
+
+var app = new Vue({
+    el: '.app',
+    data: {
+        word: '',
+        name: '',
+        figureOfSpeech: '',
+    },
+    methods: {
+        getwordfromdict(word) {
+            fetch(`http://localhost:3000/oxfordrequest/${this.word}`)
+                .then(response => (response.ok ? response.json() : Promise.reject(response)))
+                .then(resp => {
+                    console.log("We received a response from Oxford!");
+                    console.log(resp);
+                    this.name = resp.results[0].id
+                    this.figureOfSpeech = resp.results[0].lexicalEntries[0].lexicalCategory.id
+                })
+            this.word = '';
+        }
+    }
+});
+
+
